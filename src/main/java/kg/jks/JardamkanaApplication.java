3d2ce707630bcbee516d1c61d83faa589c663899
Ermek Abyshev <ermek.abyshev@gmail.com>
2017-12-25 03:24:37 +0300

    Initial commit

---
+++ src/main/java/kg/jks/JardamkanaApplication.java
@@ -0,0 +1,12 @@
+package kg.jks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JardamkanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JardamkanaApplication.class, args);
+	}
+}
